4f81afb499f9bf878461ff2a490ffe4a19f80c4b
Ahmad Fozooni <afozoonitr@gmail.com>
2022-12-05 15:10:53 +0330

    Fix Loading Cancel Button from static 'aria-label' to dynamic from translate files

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -16,7 +16,7 @@
         <path d="M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z"></path>
       </svg>
     </span>
-    <span aria-label="Cancel" data-microtip-position="bottom-right" role="tooltip" v-else>
+    <span :aria-label="t('Cancel')" data-microtip-position="bottom-right" role="tooltip" v-else>
       <svg @click="emitter.emit('vf-fetch-abort')" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 p-1 rounded text-slate-700 hover:bg-neutral-300 dark:text-neutral-200 dark:hover:bg-gray-700 cursor-pointer">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
