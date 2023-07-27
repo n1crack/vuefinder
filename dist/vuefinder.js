97f040117e981b2173239db5c96d75a028cdf788
Yusuf Özdemir <yusuf@ozdemir.be>
2023-07-27 09:01:37 +0300

    version update

--- dist/vuefinder.js
+++ dist/vuefinder.js
@@ -3139,7 +3139,7 @@ const Ji = { class: "relative flex-auto flex flex-col overflow-hidden" }, Qi = {
       Ce(Un)
     ]));
   }
-}), Va = "1.2.1", Ba = { class: "p-1 text-xs border-t border-neutral-300 dark:border-gray-700/50 flex justify-between select-none" }, za = { class: "flex leading-5 items-center" }, Ha = ["aria-label"], Ua = /* @__PURE__ */ f("svg", {
+}), Va = "1.2.2", Ba = { class: "p-1 text-xs border-t border-neutral-300 dark:border-gray-700/50 flex justify-between select-none" }, za = { class: "flex leading-5 items-center" }, Ha = ["aria-label"], Ua = /* @__PURE__ */ f("svg", {
   xmlns: "http://www.w3.org/2000/svg",
   class: "h-5 w-5",
   fill: "none",
