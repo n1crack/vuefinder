efd570139ae136394ca2bdb843258d03d39e3619
Andreas Lundgren <andreas.lundgren@qlucore.com>
2023-10-18 09:35:25 +0200

    Adding Hindi language

--- src/components/Statusbar.vue
+++ src/components/Statusbar.vue
@@ -24,13 +24,14 @@
               class="py-0.5 text-sm text-slate-500 dark:text-neutral-50 dark:bg-gray-700 rounded pl-2 pr-8 mr-3">
         <option value="" disabled>{{ t('Language') }}</option>
         <option value="en">English</option>
-        <option value="fr">French</option>
-        <option value="de">German</option>
-        <option value="tr">Hebrew</option>
-        <option value="fa">Persian</option>
-        <option value="ru">Russian</option>
-        <option value="sv">Swedish</option>
-        <option value="tr">Turkish</option>
+        <option value="fr">French (Français)</option>
+        <option value="de">German (Deutsch)</option>
+        <option value="tr">Hebrew (עִברִית)</option>
+        <option value="hi">Hindi (हिंदी)</option>
+        <option value="fa">Persian (فارسی)</option>
+        <option value="ru">Russian (Pусский)</option>
+        <option value="sv">Swedish (Svenska)</option>
+        <option value="tr">Turkish (Türkçe)</option>
       </select>
 
       <span class="mr-1" :aria-label="t('About')" data-microtip-position="top-left" role="tooltip" @click="emitter.emit('vf-modal-show', {type:'message', title:'Vuefinder ' + version, message: t('Vuefinder is a file manager component for vue 3.')})">
