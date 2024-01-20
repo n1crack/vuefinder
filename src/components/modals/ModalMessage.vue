bc2f18f0a309a526106e19bd3acee31b8601c557
DreamlandOwO <90253151+DreamlandOwO@users.noreply.github.com>
2024-01-20 21:59:30 +0800

    Fix locale messages

--- src/components/modals/ModalMessage.vue
+++ src/components/modals/ModalMessage.vue
@@ -9,7 +9,7 @@
       <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
         <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title">{{  selection?.title ?? 'Title' }}</h3>
         <div class="mt-2">
-          <p class="text-sm text-gray-500">{{  selection?.message ?? 'Message' }}.</p>
+          <p class="text-sm text-gray-500">{{  selection?.message ?? 'Message' }}</p>
         </div>
       </div>
     </div>
