f8e50b52dadc6bdc1454d542024de5e2c9f44e9f
Yusuf Özdemir <yusuf@ozdemir.be>
2024-01-23 20:28:43 +0300

    update example

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       id='my_vuefinder'
       :max-file-size="maxFileSize"
       :url="url"
+      @select="handleSelect"
     />
   </div>
 </template>
@@ -14,7 +15,14 @@ import { ref } from 'vue';
 // CHANGE ME, Url for development server endpoint
 const url = ref("http://localhost:8005/")
 const maxFileSize = ref("500MB")
-</script>
+
+// an example how to show selected files, outside of vuefinder
+// you can create a ref object and assign the items to it,
+// then with a button click, you can get the selected items easily
+const handleSelect = (selection) => {
+  console.log(selection)
+}
+</script> 
 
 <style>
 body {
