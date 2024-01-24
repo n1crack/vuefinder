7227282cd6a4256372d2eaa8d93927beb08529eb
DreamlandOwO <90253151+DreamlandOwO@users.noreply.github.com>
2024-01-24 18:33:27 +0800

    add headers to request transformer

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -4,12 +4,14 @@ export const csrf = document.querySelector('meta[name="csrf-token"]')?.getAttrib
  * @typedef RequestTransformParams
  * @property {String} url
  * @property {String} method
+ * @property {Record<String, String>} headers
  * @property {*} params
  */
 /**
  * @typedef RequestTransformResult
  * @property {?String} url
  * @property {?Object} params
+ * @property {?Record<String, String>} headers
  */
 /**
  * @callback RequestTransformer
@@ -20,35 +22,48 @@ export const csrf = document.querySelector('meta[name="csrf-token"]')?.getAttrib
  * Requester
  * @param {String} url
  * @param {Object} config
- * @param {String} config.method
+ * @param {'get'|'post'|'put'|'patch'|'delete'} config.method
  * @param {?Object} config.params
  * @param {?Boolean} config.json
  * @param {?AbortSignal} config.signal
  * @param {?RequestTransformer} config.requestTransformer
  */
 export default (url, { method = 'get', params = {}, json = true, signal = null, requestTransformer = null }) => {
-    if (requestTransformer != null) {
-        const transformResult = requestTransformer({ url, method, params: params || {} });
-        if (transformResult.url != null) {
-            url = transformResult.url
-        }
-        if (transformResult.params != null) {
-            params = transformResult.params
+    /** @type {Record<String, String>} */
+    let headers = {};
+    if (method !== 'get') {
+        if (csrf) {
+            headers['X-CSRF-Token'] = csrf;
         }
     }
-    const init = {method: method};
+    if (requestTransformer != null) {
+        const transformResult = requestTransformer({
+            url,
+            method,
+            headers,
+            params: params || {},
+        });
+        if (transformResult.url != null) {
+            url = transformResult.url;
+        }
+        if (transformResult.params != null) {
+            params = transformResult.params;
+        }
+        if (transformResult.headers != null) {
+            headers = transformResult.headers;
+        }
+    }
+    const init = {
+        method,
+        headers,
+    };
     init.signal = signal;
 
     if (method === 'get') {
         url += '?' + new URLSearchParams(params);
     } else {
-        init.headers = {};
 
-        if (csrf) {
-            init.headers['X-CSRF-Token'] = csrf;
-        }
-
-        let formData = new FormData();
+        const formData = new FormData();
 
         for (const [key, value] of Object.entries(params)) {
             formData.append(key, value);
