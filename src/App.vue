a4896f5241290c0e3ffabee52366d963f65445f5
Yusuf Özdemir <yusuf@ozdemir.be>
2024-01-27 13:53:30 +0300

    update example to demonstrate selecting items

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@
       :url="url"
       @select="handleSelect"
     />
+
+    <button class="btn" @click="handleButton" :disabled="!selectedFiles.length">Show Selected</button>
+
+    <div v-show="selectedFiles.length">
+      <h3>Selected Files</h3>
+      <ul>
+        <li v-for="file in selectedFiles" :key="file.path">
+          {{ file.path }}
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -16,12 +28,19 @@ import { ref } from 'vue';
 const url = ref("http://localhost:8005/")
 const maxFileSize = ref("500MB")
 
+
+const selectedFiles = ref([]);
 // an example how to show selected files, outside of vuefinder
 // you can create a ref object and assign the items to it,
 // then with a button click, you can get the selected items easily
 const handleSelect = (selection) => {
-  console.log(selection)
+  selectedFiles.value = selection
 }
+
+const handleButton = () => {
+  console.log(selectedFiles.value)
+}
+
 </script> 
 
 <style>
@@ -32,5 +51,15 @@ body {
 .wrapper {
   width: 800px;
   margin: 100px auto;
+}
+.btn{
+  display: block;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background: #fff;
+  cursor: pointer;
+  outline: none;
 }
 </style>
