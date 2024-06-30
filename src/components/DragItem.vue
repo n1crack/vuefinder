529f7d130f2de824d6e304b6c8d97711668b1db1
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:41:31 -0400

    Update DragItem.vue

--- src/components/DragItem.vue
+++ src/components/DragItem.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="absolute -z-50 -top-96">
+  <div class="vuefinder__drag-item__container">
     <DragSVG />
-    <div class="text-neutral-700 dark:text-neutral-300 p-1 absolute text-center top-4 right-[-2rem] md:top-5 md:right-[-2.4rem] z-20 text-xs">{{ props.count }}</div>
+    <div class="vuefinder__drag-item__count">{{ props.count }}</div>
   </div>
 </template>
 
+<style>
+.vuefinder__drag-item__container {
+  @apply absolute -z-50 -top-96;
+}
+
+.vuefinder__drag-item__count {
+  @apply text-neutral-700 dark:text-neutral-300 p-1 absolute text-center top-4 right-[-2rem] md:top-5 md:right-[-2.4rem] z-20 text-xs;
+}
+</style>
 
 <script setup>
 import { defineProps } from 'vue';
