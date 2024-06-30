7b4d29ca89351a145e7c5b865e10766dd3c156fc
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:46:22 -0400

    Update Video.vue

--- src/components/previews/Video.vue
+++ src/components/previews/Video.vue
@@ -1,15 +1,26 @@
 <template>
-<div>
-    <h3 class="mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title"
-         :title="app.modal.data.item.path">{{ app.modal.data.item.basename }}</h3>
-  <div>
-    <video class="w-full aspect-video" preload controls>
-      <source :src="getVideoUrl()" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
+  <div class="vuefinder__video-preview">
+    <h3 class="vuefinder__video-preview__title" id="modal-title" :title="app.modal.data.item.path">
+      {{ app.modal.data.item.basename }}
+    </h3>
+    <div>
+      <video class="vuefinder__video-preview__video" preload controls>
+        <source :src="getVideoUrl()" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
   </div>
-</div>
 </template>
+
+<style>
+.vuefinder__video-preview__title {
+  @apply mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400;
+}
+
+.vuefinder .vuefinder__video-preview__video {
+  @apply w-full aspect-video;
+}
+</style>
 
 <script setup>
 import {inject, onMounted} from 'vue';
