6f8d042564b58f40ed4d4fc8c013b1d0910d3c52
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:50:33 -0400

    Update Audio.vue

--- src/components/previews/Audio.vue
+++ src/components/previews/Audio.vue
@@ -1,13 +1,26 @@
 <template>
-  <h3 class="mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title"
-         :title="app.modal.data.item.path">{{ app.modal.data.item.basename }}</h3>
-  <div>
-      <audio class="w-full" controls>
-          <source :src="getAudioUrl()" type="audio/mpeg">
-          Your browser does not support the audio element.
+  <div class="vuefinder__audio-preview">
+    <h3 class="vuefinder__audio-preview__title" id="modal-title" :title="app.modal.data.item.path">
+      {{ app.modal.data.item.basename }}
+    </h3>
+    <div>
+      <audio class="vuefinder__audio-preview__audio" controls>
+        <source :src="getAudioUrl()" type="audio/mpeg">
+        Your browser does not support the audio element.
       </audio>
+    </div>
   </div>
 </template>
+
+<style>
+.vuefinder__audio-preview__title {
+  @apply mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400;
+}
+
+.vuefinder .vuefinder__audio-preview__audio {
+  @apply w-full;
+}
+</style>
 
 <script setup>
 
