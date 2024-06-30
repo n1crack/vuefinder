75e752c36fc05b9164a5e5c0bebbba3e4435f9ae
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:05:20 -0400

    Update Toast.vue

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,20 +1,51 @@
 <template>
-  <div
-      :class="fullScreen.value ?  'fixed' : 'absolute'"
-      class="max-w-fit flex flex-col bottom-0 left-1/2 -translate-x-1/2 z-10">
+  <div :class="['vuefinder__toast', fullScreen.value ? 'vuefinder__toast--fixed' : 'vuefinder__toast--absolute']">
     <transition-group
-        name="vf-toast-item"
-        leave-active-class="transition-all duration-1000"
-        leave-to-class="opacity-0"
+        name="vuefinder__toast-item"
+        enter-active-class="vuefinder__toast-item--enter-active"
+        leave-active-class="vuefinder__toast-item--leave-active"
+        leave-to-class="vuefinder__toast-item--leave-to"
     >
-      <div v-for="(message, index) in messageQueue" @click="removeItem(index)" :key="message"
-           :class="getTypeClass(message.type)"
-           class="inline-block mx-auto my-0.5 py-0.5 px-2 min-w-max bg-gray-50 dark:bg-gray-600 border text-xs sm:text-sm rounded cursor-pointer ">
+      <div v-for="(message, index) in messageQueue"
+           :key="index"
+           @click="removeItem(index)"
+           :class="['vuefinder__toast__message', getTypeClass(message.type)]">
          {{ message.label }}
       </div>
     </transition-group>
   </div>
 </template>
+
+<style>
+.vuefinder__toast {
+  @apply max-w-fit flex flex-col bottom-0 left-1/2 -translate-x-1/2 z-10;
+}
+
+.vuefinder__toast--fixed {
+  @apply fixed;
+}
+
+.vuefinder__toast--absolute {
+  @apply absolute;
+}
+
+.vuefinder__toast__message {
+  @apply inline-block mx-auto my-0.5 py-0.5 px-2 min-w-max bg-gray-50 dark:bg-gray-600 border text-xs sm:text-sm rounded cursor-pointer;
+}
+
+/* Transition classes */
+.vuefinder__toast-item--enter-active {
+  @apply transition-all duration-1000;
+}
+
+.vuefinder__toast-item--leave-active {
+  @apply transition-all duration-1000 opacity-0;
+}
+
+.vuefinder__toast-item--leave-to {
+  @apply opacity-0;
+}
+</style>
 
 <script setup>
 import {inject, ref} from 'vue';
