441f8d65e04f12bb3c95a784074c08c9d2595937
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:50:00 -0400

    Update Default.vue

--- src/components/previews/Default.vue
+++ src/components/previews/Default.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="flex">
-    <h3 class="mb-2 text-lg leading-6 font-medium text-gray-900 dark:text-gray-400" id="modal-title"
-         :title="app.modal.data.item.path">{{ app.modal.data.item.basename }}</h3>
+  <div class="vuefinder__default-preview">
+    <div class="vuefinder__default-preview__header">
+      <h3 class="vuefinder__default-preview__title" id="modal-title" :title="app.modal.data.item.path">
+        {{ app.modal.data.item.basename }}
+      </h3>
+    </div>
+    <div></div>
   </div>
-  <div></div>
 </template>
+
+<style>
+.vuefinder__default-preview {
+  @apply flex flex-col;
+}
+
+.vuefinder__default-preview__header {
+  @apply flex mb-2;
+}
+
+.vuefinder__default-preview__title {
+  @apply text-lg leading-6 font-medium text-gray-900 dark:text-gray-400;
+}
+</style>
 
 <script setup>
 
