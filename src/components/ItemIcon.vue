bc46b865c9fbf1420e8b5f29269a72ee7ca9eef1
Hugh Saffar <hughsaffar@gmail.com>
2024-06-30 19:25:08 -0400

    Update ItemIcon.vue

--- src/components/ItemIcon.vue
+++ src/components/ItemIcon.vue
@@ -1,9 +1,19 @@
 <template>
-  <span>
-    <FolderSVG v-if="type === 'dir'" :class="{'h-5 w-5': small, 'h-10 w-10 md:h-12 md:w-12 m-auto': !small}" />
-    <FileSVG v-else :class="{'h-5 w-5': small, 'h-10 w-10 md:h-12 md:w-12 m-auto': !small}" />
+  <span class="vuefinder__item-icon">
+    <FolderSVG v-if="type === 'dir'" :class="small ? 'vuefinder__item-icon--small' : 'vuefinder__item-icon--large'" />
+    <FileSVG v-else :class="small ? 'vuefinder__item-icon--small' : 'vuefinder__item-icon--large'" />
   </span>
 </template>
+
+<style>
+.vuefinder__item-icon--small {
+  @apply h-5 w-5;
+}
+
+.vuefinder__item-icon--large {
+  @apply h-10 w-10 md:h-12 md:w-12 m-auto;
+}
+</style>
 
 <script setup>
 import {defineProps} from "vue";
