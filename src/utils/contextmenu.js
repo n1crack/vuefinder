ff202cb76ab848545c89b0ea93293ad757d0e281
Andreas Bichinger <andreas.bichinger@gmail.com>
2025-07-14 16:36:41 +0200

    Refactor context menu items
    Add `id` to `Item`
    
    BREAKING CHANGE: Remove `SimpleItem`

--- src/utils/contextmenu.js
+++ src/utils/contextmenu.js
@@ -7,6 +7,29 @@ import ModalUnarchive from "../components/modals/ModalUnarchive.vue";
 import ModalRename from "../components/modals/ModalRename.vue";
 import ModalDelete from "../components/modals/ModalDelete.vue";
 
+const TARGET = {
+  none: 'none',
+  one: 'one',
+  many: 'many',
+}
+
+export const ContextMenuIds = {
+  newfolder: "newfolder",
+  selectAll: "selectAll",
+  pinFolder: "pinFolder",
+  unpinFolder: "unpinFolder",
+  delete: "delete",
+  refresh: "refresh",
+  preview: "preview",
+  open: "open",
+  openDir: "openDir",
+  download: "download",
+  download_archive: "download_archive",
+  archive: "archive",
+  unarchive: "unarchive",
+  rename: "rename",
+}
+
 /**
  * @typedef {typeof import('../ServiceContainer.js')['default']} ServiceContainer
  * @typedef {ReturnType<ServiceContainer>} App
@@ -20,6 +43,7 @@ import ModalDelete from "../components/modals/ModalDelete.vue";
  * 
  * 
  * @typedef Item
+ * @prop {string} id
  * @prop {(i18n: App['i18n']) => string} title
  * @prop {(app: App, selectedItems: DirEntry[]) => void} action
  * @prop {(app: App, selectedItems: DirEntry[]) => void} [link]
@@ -31,69 +55,52 @@ import ModalDelete from "../components/modals/ModalDelete.vue";
  * @prop {Item['link']} [link]
  * @prop {string} [key]
  * 
- * @typedef SimpleItemOptions
+ * @typedef ShowOptions
  * @prop {boolean} needsSearchQuery (default: `false`)
- * @prop {null|'one'|'many'} target (default: `'one'`)
+ * @prop {keyof typeof TARGET} target
  * @prop {string} targetType
  * @prop {string} mimeType
  * @prop {string} feature
- * @prop {Item['show']} show
+ * 
  */
 
 /**
- * @class
- * @implements {Item}
+ * @param {MenuContext} ctx
+ * @returns {ShowOptions['target']}
  */
-export class SimpleItem {
-
-  /**
-   * 
-   * @param {Item['title']} title 
-   * @param {Item['action']} action 
-   * @param {Item['link']} link
-   * @param {Partial<SimpleItemOptions>} options 
-   */
-  constructor(title, action, link, options) {
-    this.title = title
-    this.action = action
-    this.link = link
-    this.options = Object.assign(
-      {
-        needsSearchQuery: false, 
-        target: 'one',
-      }, 
-      options,
-    )
+function getTarget(ctx) {
+  if (ctx.items.length > 1 && ctx.items.some((item) => item.path === ctx.target?.path)) {
+    return 'many'
   }
+  return ctx.target ? 'one' : 'none'
+}
 
-  /**
-   * @type {Item['show']}
-   */
-  show(app, ctx) {
-    /** @type {(ctx: MenuContext) => SimpleItemOptions['target']} */
-    const actualTarget = (ctx) => {
-      if (ctx.items.length > 1 && ctx.items.some((item) => item.path === ctx.target?.path)) {
-        return 'many'
-      }
-      return ctx.target ? 'one' : null
-    }
+/**
+ * @param {Partial<ShowOptions>} options 
+ * @returns {Item['show']}
+ */
+function showIf(options) {
+  options = Object.assign(
+    {
+      needsSearchQuery: false, 
+    }, 
+    options,
+  )
 
-    if (this.options.needsSearchQuery !== !!ctx.searchQuery) {
+  return (app, ctx) => {
+    if (options.needsSearchQuery !== !!ctx.searchQuery) {
       return false
     }
-    if (this.options.target !== undefined && this.options.target !== actualTarget(ctx)) {
+    if (options.target !== undefined && options.target !== getTarget(ctx)) {
       return false
     }
-    if (this.options.targetType !== undefined && this.options.targetType !== ctx.target?.type) {
+    if (options.targetType !== undefined && options.targetType !== ctx.target?.type) {
       return false
     }
-    if (this.options.mimeType !== undefined && this.options.mimeType !== ctx.target?.mime_type) {
+    if (options.mimeType !== undefined && options.mimeType !== ctx.target?.mime_type) {
       return false
     }
-    if (this.options.feature !== undefined && !app.features.includes(this.options.feature)) {
-      return false
-    }
-    if (this.options.show !== undefined && !this.options.show(app, ctx)) {
+    if (options.feature !== undefined && !app.features.includes(options.feature)) {
       return false
     }
 
@@ -102,78 +109,39 @@ export class SimpleItem {
 }
 
 /**
- * 
- * @param {ItemTemplate[]} templates 
- * @param {SimpleItemOptions} options
- * @returns {Item[]}
+ * @param  {...Item['show']} showFns
+ * @returns {Item['show']}
  */
-function itemBundle(templates, options) {
-  return templates.map((template) => {
-    return new SimpleItem(template.title, template.action, template.link, {
-      ...options,
-      feature: template.key
-    })
-  })
+function showIfAny(...showFns) {
+  return (app, ctx) => {
+    for (let fn of showFns) {
+      if (fn(app, ctx)) {
+        return true
+      }
+    }
+    return false
+  }
 }
 
-/** @type {Record<string, ItemTemplate>} */
-const templateMap = {
-  newfolder: {
-    key: FEATURES.NEW_FOLDER,
-    title: ({t}) => t('New Folder'),
-    action: (app) => app.modal.open(ModalNewFolder),
-  },
-  selectAll: {
-    title: ({t}) => t('Select All'),
-    action: (app) => app.dragSelect.selectAll(),
-  },
-  pinFolder: {
-    title: ({t}) => t('Pin Folder'),
-    action: (app, selectedItems) => {
-        app.pinnedFolders = app.pinnedFolders.concat(selectedItems.value);
-        app.storage.setStore('pinned-folders', app.pinnedFolders);
-    },
-  },
+/**
+ * @param  {...Item['show']} showFns
+ * @returns {Item['show']}
+ */
+function showIfAll(...showFns) {
+  return (app, ctx) => {
+    for (let fn of showFns) {
+      if (!fn(app, ctx)) {
+        return false
+      }
+    }
+    return true
+  }
+}
 
-  unpinFolder: {
-    title: ({t}) => t('Unpin Folder'),
-    action: (app, selectedItems) => {
-        app.pinnedFolders = app.pinnedFolders.filter(fav => !selectedItems.value.find(item => item.path === fav.path));
-        app.storage.setStore('pinned-folders', app.pinnedFolders);
-    },
-  },
-  delete: {
-    key: FEATURES.DELETE,
-    title: ({t}) => t('Delete'),
-    action: (app, selectedItems) => {
-      app.modal.open(ModalDelete, {items: selectedItems});
-    },
-  },
-  refresh: {
-    title: ({t}) => t('Refresh'),
-    action: (app) => {
-      app.emitter.emit('vf-fetch', {params: {q: 'index', adapter: app.fs.adapter, path: app.fs.data.dirname}});
-    },
-  },
-  preview: {
-    key: FEATURES.PREVIEW,
-    title: ({t}) => t('Preview'),
-    action: (app, selectedItems) => app.modal.open(ModalPreview, {adapter: app.fs.adapter, item: selectedItems.value[0]}),
-  },
-  open: {
-    title: ({t}) => t('Open'),
-    action: (app, selectedItems) => {
-      app.emitter.emit('vf-search-exit');
-      app.emitter.emit('vf-fetch', {
-        params: {
-          q: 'index',
-          adapter: app.fs.adapter,
-          path: selectedItems.value[0].path
-        }
-      });
-    },
-  },
-  openDir: {
+/** @type {Item[]} */
+export const menuItems = [
+  {
+    id: ContextMenuIds.openDir,
     title: ({t}) => t('Open containing folder'),
     action: (app, selectedItems) => {
       app.emitter.emit('vf-search-exit');
@@ -185,61 +153,125 @@ const templateMap = {
         }
       });
     },
+    show: showIf({
+      target: 'one',
+      needsSearchQuery: true,
+    })
   },
-  download: {
-    key: FEATURES.DOWNLOAD,
+  {
+    id: ContextMenuIds.refresh,
+    title: ({t}) => t('Refresh'),
+    action: (app) => {
+      app.emitter.emit('vf-fetch', {params: {q: 'index', adapter: app.fs.adapter, path: app.fs.data.dirname}});
+    },
+    show: showIfAny(
+      showIf({target: 'none'}), 
+      showIf({target: 'many'})
+    )
+  },
+  {
+    id: ContextMenuIds.selectAll,
+    title: ({t}) => t('Select All'),
+    action: (app) => app.dragSelect.selectAll(),
+    show: showIf({target: 'none'})
+  },
+  {
+    id: ContextMenuIds.newfolder,
+    title: ({t}) => t('New Folder'),
+    action: (app) => app.modal.open(ModalNewFolder),
+    show: showIf({target: 'none', feature: FEATURES.NEW_FOLDER})
+  },
+  {
+    id: ContextMenuIds.open,
+    title: ({t}) => t('Open'),
+    action: (app, selectedItems) => {
+      app.emitter.emit('vf-search-exit');
+      app.emitter.emit('vf-fetch', {
+        params: {
+          q: 'index',
+          adapter: app.fs.adapter,
+          path: selectedItems.value[0].path
+        }
+      });
+    },
+    show: showIf({target: 'one', targetType: 'dir'})
+  },
+  {
+    id: ContextMenuIds.pinFolder,
+    title: ({t}) => t('Pin Folder'),
+    action: (app, selectedItems) => {
+        app.pinnedFolders = app.pinnedFolders.concat(selectedItems.value);
+        app.storage.setStore('pinned-folders', app.pinnedFolders);
+    },
+    show: showIfAll(
+      showIf({target: 'one', targetType: 'dir'}), 
+      (app, ctx) => app.pinnedFolders.findIndex((item) => item.path === ctx.target?.path) === -1,
+    )
+  },
+  {
+    id: ContextMenuIds.unpinFolder,
+    title: ({t}) => t('Unpin Folder'),
+    action: (app, selectedItems) => {
+        app.pinnedFolders = app.pinnedFolders.filter(fav => !selectedItems.value.find(item => item.path === fav.path));
+        app.storage.setStore('pinned-folders', app.pinnedFolders);
+    },
+    show: showIfAll(
+      showIf({target: 'one', targetType: 'dir'}), 
+      (app, ctx) => app.pinnedFolders.findIndex((item) => item.path === ctx.target?.path) !== -1,
+    )
+  },
+  {
+    id: ContextMenuIds.preview,
+    title: ({t}) => t('Preview'),
+    action: (app, selectedItems) => app.modal.open(ModalPreview, {adapter: app.fs.adapter, item: selectedItems.value[0]}),
+    show: showIfAll(
+      showIf({target: 'one', feature: FEATURES.PREVIEW}), 
+      (app, ctx) => ctx.target?.type !== 'dir'
+    )
+  },
+  {
+    id: ContextMenuIds.download,
     link: (app, selectedItems) => app.requester.getDownloadUrl(app.fs.adapter, selectedItems.value[0]),
     title: ({t}) => t('Download'),
     action: () => {},
+    show: showIfAll(
+      showIf({target: 'one', feature: FEATURES.DOWNLOAD}), 
+      (app, ctx) => ctx.target?.type !== 'dir'
+    )
   },
-  archive: {
-    key: FEATURES.ARCHIVE,
-    title: ({t}) => t('Archive'),
-    action: (app, selectedItems) => app.modal.open(ModalArchive, {items: selectedItems}),
-  },
-  unarchive: {
-    key: FEATURES.UNARCHIVE,
-    title: ({t}) => t('Unarchive'),
-    action: (app, selectedItems) => app.modal.open(ModalUnarchive, {items: selectedItems}),
-  },
-  rename: {
-    key: FEATURES.RENAME,
+  {
+    id: ContextMenuIds.rename,
     title: ({t}) => t('Rename'),
     action: (app, selectedItems) => app.modal.open(ModalRename, {items: selectedItems}),
-  }
-};
-
-/** @type {Item[]} */
-export const menuItems = [
-  ...itemBundle([templateMap.openDir], {
-    needsSearchQuery: true,
-  }),
-  ...itemBundle([templateMap.refresh, templateMap.selectAll, templateMap.newfolder], {
-    target: null,
-  }),
-  ...itemBundle([templateMap.refresh, templateMap.archive, templateMap.delete], {
-    target: 'many'
-  }),
-  ...itemBundle([templateMap.open], {
-    targetType: 'dir',
-  }),
-  ...itemBundle([templateMap.unpinFolder], {
-    targetType: 'dir',
-    show: (app, ctx) => app.pinnedFolders.findIndex((item) => item.path === ctx.target?.path) !== -1
-  }),
-  ...itemBundle([templateMap.pinFolder], {
-    targetType: 'dir',
-    show: (app, ctx) => app.pinnedFolders.findIndex((item) => item.path === ctx.target?.path) === -1
-  }),
-  ...itemBundle([templateMap.preview, templateMap.download], {
-    show: (app, ctx) => ctx.target?.type !== 'dir'
-  }),
-  ...itemBundle([templateMap.rename], {numItems: 'one',}),
-  ...itemBundle([templateMap.unarchive], {
-    mimeType: 'application/zip',
-  }),
-  ...itemBundle([templateMap.archive], {
-    show: (app, ctx) => ctx.target?.mime_type !== 'application/zip'
-  }),
-  ...itemBundle([templateMap.delete], {}),
+    show: showIf({target: 'one', feature: FEATURES.RENAME})
+  },
+  { 
+    id: ContextMenuIds.archive,
+    title: ({t}) => t('Archive'),
+    action: (app, selectedItems) => app.modal.open(ModalArchive, {items: selectedItems}),
+    show: showIfAny(
+      showIf({target: 'many', feature: FEATURES.ARCHIVE}),
+      showIfAll(
+        showIf({target: 'one', feature: FEATURES.ARCHIVE}), 
+        (app, ctx) => ctx.target?.mime_type !== 'application/zip'
+      )
+    ) 
+  },
+  {
+    id: ContextMenuIds.unarchive,
+    title: ({t}) => t('Unarchive'),
+    action: (app, selectedItems) => app.modal.open(ModalUnarchive, {items: selectedItems}),
+    show: showIf({target: 'one', feature: FEATURES.UNARCHIVE, mimeType: 'application/zip'})
+  },
+  {
+    id: ContextMenuIds.delete,
+    title: ({t}) => t('Delete'),
+    action: (app, selectedItems) => {
+      app.modal.open(ModalDelete, {items: selectedItems});
+    },
+    show: showIfAny(
+      showIf({feature: FEATURES.DELETE, target: 'one'}),
+      showIf({feature: FEATURES.DELETE, target: 'many'}),
+    )
+  },
 ]
